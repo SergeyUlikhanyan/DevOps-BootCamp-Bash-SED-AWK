9d046fad6cc9b7bd95c63e1ed9385ce56ec05c15
Seryozha Ulikhanyan <seryozha_ulikhanyan@epam.com>
2022-12-23 17:49:12 +0400

    changed two files

--- sedawk.sh
+++ sedawk.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-# Put you code here
+cp passwd passwd_new
+awk 'BEGIN{OFS=FS=":"}$1=="saned"{$7="/bin/bash"}{print}' passwd_new > tmp && mv tmp passwd_new
+sed -i '/avahi/s/\/usr\/sbin\/nologin/\/bin\/bash/' passwd_new
+awk 'BEGIN{FS=":"; OFS=":"} {print $1,$3,$5,$7}' passwd_new > tmp && mv tmp passwd_new
+sed -i '/daemon/d' passwd_new
+awk 'BEGIN{OFS=FS=":"}$2%2==0{$4="/bin/zsh"}{print}' passwd_new > tmp && mv tmp passwd_new
